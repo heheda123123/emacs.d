888f6e173b451894bcb96557e15f682a7b52b883
xpxz <97490782+heheda123123@users.noreply.github.com>
2023-06-29 18:38:33 +0800

    Create early-init.el

---
+++ early-init.el
@@ -0,0 +1,16 @@
+;; 初始化界面的最先调用
+(setq inhibit-startup-message t)
+(setq default-frame-alist
+      '((menu-bar-lines . 0)
+        (tool-bar-lines . 0)
+        (horizontal-scroll-bars)
+        (vertical-scroll-bars)))
+(defun xiaozhu/reset-frame-size (&optional frame)
+    (interactive)
+    (when frame
+      (select-frame frame))
+    (set-frame-width (selected-frame) 200)
+    (set-frame-height (selected-frame) 50))
+(xiaozhu/reset-frame-size)
+(add-hook 'after-make-frame-functions 'xiaozhu/reset-frame-size)
+(global-display-line-numbers-mode)
