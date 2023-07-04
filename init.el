d26c3630a16a7cb062df49ff5fbb0f68e016ff0e
xpxz <97490782+heheda123123@users.noreply.github.com>
2023-07-04 20:41:37 +0800

    Update init.el

--- init.el
+++ init.el
@@ -167,9 +167,10 @@
 
 
 (use-package yasnippet
+  :defer 8
   :ensure t
-  :defer 4
-  :config (yas-global-mode 1))
+  :config (yas-global-mode t)
+  )
 (use-package yasnippet-snippets
   :ensure t
   :after (yasnippet))
@@ -179,25 +180,37 @@
 
 (add-to-list 'load-path "~/emacs-plugin/lsp-bridge")
 (require 'lsp-bridge)
+(setq acm-enable-doc nil)
 (global-lsp-bridge-mode)
+
+(add-to-list 'load-path "~/emacs-plugin/auto-save") ; add auto-save to your load-path
+(require 'auto-save)
+(auto-save-enable)
+(setq auto-save-silent t)   ; quietly save
+(setq auto-save-delete-trailing-whitespace t)  ; automatically delete spaces at the end of the line when saving
+(setq auto-save-disable-predicates
+      '((lambda ()
+	  (string-suffix-p
+	   "gpg"
+	   (file-name-extension (buffer-name)) t))))
 
 (use-package counsel-etags
   :ensure t
   :bind (("C-]" . counsel-etags-find-tag-at-point))
   :init
-  (add-hook 'prog-mode-hook
-        (lambda ()
-          (add-hook 'after-save-hook
-            'counsel-etags-virtual-update-tags 'append 'local)))
+  (lambda ()
+    (add-hook 'after-save-hook
+              'counsel-etags-virtual-update-tags 'append 'local))
   :config
   (setq counsel-etags-update-interval 60)
   (push "build" counsel-etags-ignore-directories))
 
 
-;; (use-package dumb-jump
-;;   :ensure t
-;;   :commands (dumb-jump-go)
-;;   )
+(use-package dumb-jump
+  :ensure t
+  :config
+  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
+  )
 
 ;; (defun lsp-bridge-jump ()
 ;;   (interactive)
@@ -217,8 +230,8 @@
 ;; (require 'blink-search)
 
 
-(add-to-list 'load-path "~/emacs-plugin/color-rg") ; add color-rg to your load-path
-(require 'color-rg)
+;; (add-to-list 'load-path "~/emacs-plugin/color-rg") ; add color-rg to your load-path
+;; (require 'color-rg)
 
 (defun consult-directory-externally (file)
   (interactive)
@@ -246,11 +259,9 @@
   :commands (counsel-apropos))
 
 (use-package keycast
-  :defer 5
   :ensure t
+  :commands (keycast-header-line-mode)
   )
-
-
 
 
 (use-package evil
@@ -258,63 +269,79 @@
   :init
   (setq evil-want-C-u-scroll t)
   :config
-  (progn 
+  (progn
     (evil-mode 1)
     (setcdr evil-insert-state-map nil)
     (define-key evil-insert-state-map [escape] 'evil-normal-state)
-    (evil-set-leader '(normal visual) (kbd ","))
-    (evil-set-leader '(normal visual) (kbd "SPC") t)
-    (evil-define-key '(normal visual) 'global (kbd "RET") 'open-newline-below)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>ff") 'find-file)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>fc") 'my-open-current-directory)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>bb") 'switch-to-buffer)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>bk") 'kill-buffer)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>ss") 'consult-line)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>sn") 'consult-imenu)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>qq") 'consult-ripgrep)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>rr") 'consult-recent-file)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>hf") 'helpful-callable)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>hv") 'helpful-variable)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>hk") 'helpful-key)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>ha") 'counsel-apropos)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>hb") 'embark-bindings)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>pp") 'projectile-switch-project)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>pb") 'projectile-switch-to-buffer)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>pf") 'projectile-find-file)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>tt") 'counsel-etags-find-tag)
-    ;; (evil-define-key '(normal visual) 'global (kbd "<leader>pq") 'projectile-ripgrep)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>dd") 'dirvish)
-    (evil-define-key '(normal visual) 'global (kbd "<localleader>1") 'winum-select-window-1)
-    (evil-define-key '(normal visual) 'global (kbd "<localleader>2") 'winum-select-window-2)
-    (evil-define-key '(normal visual) 'global (kbd "<localleader>3") 'winum-select-window-3)
-    (evil-define-key '(normal visual) 'global (kbd "<localleader>4") 'winum-select-window-4)
-    (evil-define-key '(normal visual) 'global (kbd "<localleader>9") 'kill-buffer-and-window)
-    (evil-define-key '(normal visual) 'global (kbd "<localleader>0") 'delete-other-windows)
-    (evil-define-key '(normal visual) 'global (kbd "<localleader>/") 'split-window-right)
-    (evil-define-key '(normal visual) 'global (kbd "<localleader>-") 'split-window-below)
-    (evil-define-key '(normal visual) 'global (kbd "<localleader>'") 'avy-goto-char-timer)
-    (define-key evil-normal-state-map (kbd "gd") 'lsp-bridge-jump)
-    (define-key evil-normal-state-map (kbd "C-]") 'lsp-bridge-jump)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>md") 'lsp-bridge-find-def)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>mr") 'lsp-bridge-find-references)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>mo") 'lsp-bridge-popup-documentation)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>ma") 'lsp-bridge-diagnostic-list)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>mt") 'counsel-etags-find-tag-at-point)
-    (evil-define-key '(normal visual) 'global (kbd "<leader>mb") 'quickrun)
-    (evil-define-key '(normal visual) 'global "u" (lambda () (interactive) (if (not (fboundp 'vundo)) (evil-undo 1) (vundo) (vundo-backward 1))))
+    (evil-set-leader '(normal visual motion) (kbd ","))
+    (evil-set-leader '(normal visual motion) (kbd "SPC") t)
+    (evil-define-key '(normal visual motion) 'global (kbd "RET") 'open-newline-below)
+    ;; file
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>ff") 'find-file)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>fc") 'my-open-current-directory)
+    ;; buffer
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>bb") 'switch-to-buffer)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>bo") 'switch-to-buffer-other-window)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>bk") 'kill-buffer)
+    ;; bookmark
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>ba") 'consult-bookmark)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>bd") 'bookmark-delete)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>ss") 'consult-line)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>sn") 'consult-imenu)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>qq") 'consult-ripgrep)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>rr") 'consult-recent-file)
+    ;; help
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>hf") 'helpful-callable)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>hv") 'helpful-variable)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>hk") 'helpful-key)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>ha") 'counsel-apropos)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>hb") 'embark-bindings)
+    ;; project
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>pp") 'projectile-switch-project)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>pb") 'projectile-switch-to-buffer)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>pf") 'projectile-find-file)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>tt") 'counsel-etags-find-tag)    ;; list all tags
+    ;; (evil-define-key '(normal visual motion) 'global (kbd "<leader>pq") 'projectile-ripgrep)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>dd") 'dirvish)
+    ;; window
+    (evil-define-key '(normal visual motion) 'global (kbd "<localleader>1") 'winum-select-window-1)
+    (evil-define-key '(normal visual motion) 'global (kbd "<localleader>2") 'winum-select-window-2)
+    (evil-define-key '(normal visual motion) 'global (kbd "<localleader>3") 'winum-select-window-3)
+    (evil-define-key '(normal visual motion) 'global (kbd "<localleader>4") 'winum-select-window-4)
+    (evil-define-key '(normal visual motion) 'global (kbd "<localleader>0") 'kill-buffer-and-window)
+    (evil-define-key '(normal visual motion) 'global (kbd "<localleader>o") 'delete-other-windows)
+    (evil-define-key '(normal visual motion) 'global (kbd "<localleader>/") 'split-window-right)
+    (evil-define-key '(normal visual motion) 'global (kbd "<localleader>-") 'split-window-below)
+    (evil-define-key '(normal visual motion) 'global (kbd "<localleader>'") 'avy-goto-char-timer)
+    ;; lsp
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>md") 'lsp-bridge-find-def)    ;; go def use lsp-bridge
+    (define-key evil-normal-state-map (kbd "gd") 'lsp-bridge-find-def)
+    (define-key evil-normal-state-map (kbd "C-]") 'lsp-bridge-find-def)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>mr") 'lsp-bridge-find-references)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>mo") 'lsp-bridge-popup-documentation)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>ma") 'lsp-bridge-diagnostic-list)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>mt") 'counsel-etags-find-tag-at-point)    ;; go def use counsel-etags
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>mb") 'quickrun)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>ms") 'lsp-bridge-workspace-list-symbols)
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>mj") 'xref-find-definitions)    ;; go def use dumb-jump
+    (evil-define-key '(normal visual motion) 'global (kbd "<leader>yy") 'consult-yank-from-kill-ring)
+    (evil-define-key '(normal visual motion) 'global "u" (lambda () (interactive) (if (not (fboundp 'vundo)) (evil-undo 1) (vundo) (vundo-backward 1))))
+    (evil-define-key '(normal visual motion) 'global "n" 'evil-backward-paragraph)
+    (evil-define-key '(normal visual motion) 'global "m" 'evil-forward-paragraph)
+    (define-key evil-normal-state-map (kbd "<tab>") 'evil-switch-to-windows-last-buffer)
+
     ))
 
 
-(add-hook 'lsp-bridge-ref-mode-hook '(lambda () (evil-emacs-state))) ;; j/k 可以直接跳转到下一项
-(add-hook 'embark-collect-mode-hook '(lambda () (evil-emacs-state)))
-(add-hook 'helpful-mode-hook '(lambda () (evil-emacs-state)))
-(add-hook 'magit-mode-hook '(lambda () (evil-emacs-state)))
-(add-hook 'special-mode-hook '(lambda () (evil-emacs-state)))
 
 (use-package evil-nerd-commenter
   :after (evil)
   :ensure t
   :config (evilnc-default-hotkeys))
+
+;; (use-package evil-quickscope
+;;   :ensure t
+;;   :config (global-evil-quickscope-always-mode 1))
 
 (use-package evil-surround
   :after (evil)
@@ -333,14 +360,54 @@
     (evil-escape-mode 1)
     ))
 
-;; (use-package citre
-;;   :ensure t
-;;   :init
-;;   (require 'citre-config)
-;;   )
+(use-package shackle
+  :ensure t
+  :hook (after-init . shackle-mode)
+  :init
+  (setq shackle-rules
+	;; select 是否选中    inhibit-window-quit 按q时不关buffer    size 0到1之间控制屏幕比例    align 位置    other 是否复用当前frame的另一个window    popup 弹出新窗口而不是复用
+        ;; same 复用当前窗口    ignore 禁止显示窗口
+        '(
+	  ;; (compilation-mode              :ignore t)
+          ;; ("\\*Async Shell.*\\*" :regexp t :ignore t)
+          ;; ("*eshell*"                    :select t                          :size 0.4  :align t     :popup t)
+          ;; (helpful-mode                  :select t                          :size 0.6  :align right :popup t)
+          ;; ("*Messages*"                  :select t                          :size 0.4  :align t     :popup t)
+          ;; (magit-status-mode             :select t   :inhibit-window-quit t                         :same t)
+          ;; (magit-log-mode                :select t   :inhibit-window-quit t                         :same t)
+          ))
+  )
 
-;; (add-to-list 'load-path "~/.emacs.d")
-;; (require 'consult-citre)
+(use-package popper
+  :ensure t
+  ;; :bind (("C-`"   . popper-toggle-latest)
+  ;; 	 ("M-`"   . popper-cycle)
+  ;; 	 ("C-M-`" . popper-toggle-type))
+  :init
+  (setq popper-reference-buffers
+        '(
+	  ;; "\\*Messages\\*"
+          ;; "\\*Async Shell Command\\*"
+          ;; help-mode
+          ;; helpful-mode
+          ;; "^\\*eshell.*\\*$" eshell-mode ;; eshell as a popup
+          ;; "^\\*shell.*\\*$"  shell-mode  ;; shell as a popup
+          ;; (compilation-mode . hide)
+          )
+        )
+  (setq popper-display-control nil)
+  :config
+  (popper-mode +1)
+  (popper-echo-mode +1)
+  )
+
+
+(add-hook 'lsp-bridge-ref-mode-hook '(lambda () (evil-emacs-state))) ;; j/k 可以直接跳转到下一项
+;; (add-hook 'embark-collect-mode-hook '(lambda () (evil-emacs-state)))
+;; (add-hook 'helpful-mode-hook '(lambda () (evil-emacs-state)))
+;; (add-hook 'magit-mode-hook '(lambda () (evil-emacs-state)))
+;; (add-hook 'special-mode-hook '(lambda () (evil-emacs-state)))
+
 
 (defun smart-q ()
   "Delete window in read-only buffers, otherwise record macro."
@@ -352,18 +419,28 @@
     (call-interactively 'evil-record-macro)))
 (define-key evil-normal-state-map (kbd "q") 'smart-q)
 
-(use-package magit
-  :commands (magit-status magit)
-  :ensure t)
+;; (use-package citre
+;;   :ensure t
+;;   :init
+;;   (require 'citre-config)
+;;   )
+
+;; (add-to-list 'load-path "~/.emacs.d")
+;; (require 'consult-citre)
+
+
+;; (use-package magit
+;;   :commands (magit-status magit)
+;;   :ensure t)
 
 
 (use-package go-mode
-  :ensure t 
+  :ensure t
   :mode ("\\.go\\'" . go-mode))
 
 
 (use-package rust-mode
-  :ensure t 
+  :ensure t
   :mode ("\\.rs\\'" . rust-mode))
 
 (use-package lua-mode
@@ -372,6 +449,20 @@
   :interpreter ("lua" . lua-mode)
   )
 
+;; (use-package treesit-auto
+;;   :ensure t
+;;   :demand t
+;;   :config
+;;   (setq treesit-auto-install 'prompt)
+;;   (global-treesit-auto-mode))
+
+;; (add-to-list 'load-path "~/emacs-plugin/emacs-application-framework")
+;; (require 'eaf)
+;; (require 'eaf-browser)
+;; (require 'eaf-file-manager)
+;; (require 'eaf-git)
+;; (require 'eaf-pdf-viewer)
+;; (require 'eaf-pyqterminal)
 
 (add-to-list 'load-path "~/emacs-plugin/awesome-pair") ; add awesome-pair to your load-path
 (require 'awesome-pair)
@@ -407,35 +498,38 @@
 (define-key awesome-pair-mode-map (kbd "(") 'awesome-pair-open-round)
 (define-key awesome-pair-mode-map (kbd "[") 'awesome-pair-open-bracket)
 (define-key awesome-pair-mode-map (kbd "{") 'awesome-pair-open-curly)
-;; 写完括号里面的代码直接按右括号即可跳出括号，保证会配对
 (define-key awesome-pair-mode-map (kbd ")") 'awesome-pair-close-round)
 (define-key awesome-pair-mode-map (kbd "]") 'awesome-pair-close-bracket)
 (define-key awesome-pair-mode-map (kbd "}") 'awesome-pair-close-curly)
 (define-key awesome-pair-mode-map (kbd "=") 'awesome-pair-equal)
-;; 括号跳转
+
 (define-key awesome-pair-mode-map (kbd "%") 'awesome-pair-match-paren)
 (define-key awesome-pair-mode-map (kbd "\"") 'awesome-pair-double-quote)
 
 (define-key awesome-pair-mode-map (kbd "SPC") 'awesome-pair-space)
 (define-key awesome-pair-mode-map (kbd "RET") 'awesome-pair-newline)
-;; 删文本并保持括号配对
+
 (define-key awesome-pair-mode-map (kbd "M-o") 'awesome-pair-backward-delete)
 (define-key awesome-pair-mode-map (kbd "C-d") 'awesome-pair-forward-delete)
 (define-key awesome-pair-mode-map (kbd "C-k") 'awesome-pair-kill)
-;; 自动给当前文本加括号
+
 (define-key awesome-pair-mode-map (kbd "M-\"") 'awesome-pair-wrap-double-quote)
 (define-key awesome-pair-mode-map (kbd "M-[") 'awesome-pair-wrap-bracket)
 (define-key awesome-pair-mode-map (kbd "M-{") 'awesome-pair-wrap-curly)
 (define-key awesome-pair-mode-map (kbd "M-(") 'awesome-pair-wrap-round)
-;; 消括号
 (define-key awesome-pair-mode-map (kbd "M-)") 'awesome-pair-unwrap)
-;; 跳转
+
 (define-key awesome-pair-mode-map (kbd "M-p") 'awesome-pair-jump-right)
 (define-key awesome-pair-mode-map (kbd "M-n") 'awesome-pair-jump-left)
 (define-key awesome-pair-mode-map (kbd "M-:") 'awesome-pair-jump-out-pair-and-newline)
 
 ;; (use-package esup
 ;;   :ensure t)
+
+;; (use-package auto-highlight-symbol
+;;   :ensure t
+;;   )
+
 
 (use-package quickrun
   :commands (quickrun)
