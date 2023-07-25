54adf597bcceba02a0aa0f98240129ea851223d1
xpxz <97490782+heheda123123@users.noreply.github.com>
2023-07-25 11:20:08 +0800

    Update init.el

--- init.el
+++ init.el
@@ -62,14 +62,14 @@
 
 (add-to-list 'prog-mode-hook #'hs-minor-mode)
 
-;; consolas + 更纱黑体 SC
+;; 11, consolas + 更纱黑体 SC
 (use-package cnfonts
   :ensure t
   :config (progn
 	    (cnfonts-mode 1)
 	    (define-key cnfonts-mode-map (kbd "C--") #'cnfonts-decrease-fontsize)
 	    (define-key cnfonts-mode-map (kbd "C-=") #'cnfonts-increase-fontsize)
-	    (setq cnfonts-personal-fontnames '(() ("更纱黑体 SC" "等距更纱黑体 SC") () () ()))
+	    (setq cnfonts-personal-fontnames '(() ("更纱黑体 SC") () () ()))
 	    ))
 
 (use-package expand-region
@@ -702,50 +702,50 @@
 (define-key awesome-pair-mode-map (kbd "\"") 'awesome-pair-double-quote-my)
 
 
-(add-to-list 'load-path "~/emacs-plugin/one-key")
-(add-to-list 'load-path "~/emacs-plugin/thing-edit")
-(setq one-key-items-per-line 3)
-(require 'one-key)
-(require 'thing-edit)
-(one-key-create-menu
- "THING-EDIT"
- '(
-   ;; Copy.
-   (("w" . "Copy Word") . thing-copy-word)
-   (("s" . "Copy Symbol") . thing-copy-symbol)
-   (("m" . "Copy Email") . thing-copy-email)
-   (("f" . "Copy Filename") . thing-copy-filename)
-   (("u" . "Copy URL") . thing-copy-url)
-   (("x" . "Copy Sexp") . thing-copy-sexp)
-   (("g" . "Copy Page") . thing-copy-page)
-   (("t" . "Copy Sentence") . thing-copy-sentence)
-   (("o" . "Copy Whitespace") . thing-copy-whitespace)
-   (("i" . "Copy List") . thing-copy-list)
-   (("c" . "Copy Comment") . thing-copy-comment)
-   (("h" . "Copy Function") . thing-copy-defun)
-   (("p" . "Copy Parentheses") . thing-copy-parentheses)
-   (("l" . "Copy Line") . thing-copy-line)
-   (("a" . "Copy To Line Begin") . thing-copy-to-line-beginning)
-   (("e" . "Copy To Line End") . thing-copy-to-line-end)
-   ;; Cut.
-   (("W" . "Cut Word") . thing-cut-word)
-   (("S" . "Cut Symbol") . thing-cut-symbol)
-   (("M" . "Cut Email") . thing-cut-email)
-   (("F" . "Cut Filename") . thing-cut-filename)
-   (("U" . "Cut URL") . thing-cut-url)
-   (("X" . "Cut Sexp") . thing-cut-sexp)
-   (("G" . "Cut Page") . thing-cut-page)
-   (("T" . "Cut Sentence") . thing-cut-sentence)
-   (("O" . "Cut Whitespace") . thing-cut-whitespace)
-   (("I" . "Cut List") . thing-cut-list)
-   (("C" . "Cut Comment") . thing-cut-comment)
-   (("H" . "Cut Function") . thing-cut-defun)
-   (("P" . "Cut Parentheses") . thing-cut-parentheses)
-   (("L" . "Cut Line") . thing-cut-line)
-   (("A" . "Cut To Line Begin") . thing-cut-to-line-beginning)
-   (("E" . "Cut To Line End") . thing-cut-to-line-end)
-   )
- t)
+;; (add-to-list 'load-path "~/emacs-plugin/one-key")
+;; (add-to-list 'load-path "~/emacs-plugin/thing-edit")
+;; (setq one-key-items-per-line 3)
+;; (require 'one-key)
+;; (require 'thing-edit)
+;; (one-key-create-menu
+;;  "THING-EDIT"
+;;  '(
+;;    ;; Copy.
+;;    (("w" . "Copy Word") . thing-copy-word)
+;;    (("s" . "Copy Symbol") . thing-copy-symbol)
+;;    (("m" . "Copy Email") . thing-copy-email)
+;;    (("f" . "Copy Filename") . thing-copy-filename)
+;;    (("u" . "Copy URL") . thing-copy-url)
+;;    (("x" . "Copy Sexp") . thing-copy-sexp)
+;;    (("g" . "Copy Page") . thing-copy-page)
+;;    (("t" . "Copy Sentence") . thing-copy-sentence)
+;;    (("o" . "Copy Whitespace") . thing-copy-whitespace)
+;;    (("i" . "Copy List") . thing-copy-list)
+;;    (("c" . "Copy Comment") . thing-copy-comment)
+;;    (("h" . "Copy Function") . thing-copy-defun)
+;;    (("p" . "Copy Parentheses") . thing-copy-parentheses)
+;;    (("l" . "Copy Line") . thing-copy-line)
+;;    (("a" . "Copy To Line Begin") . thing-copy-to-line-beginning)
+;;    (("e" . "Copy To Line End") . thing-copy-to-line-end)
+;;    ;; Cut.
+;;    (("W" . "Cut Word") . thing-cut-word)
+;;    (("S" . "Cut Symbol") . thing-cut-symbol)
+;;    (("M" . "Cut Email") . thing-cut-email)
+;;    (("F" . "Cut Filename") . thing-cut-filename)
+;;    (("U" . "Cut URL") . thing-cut-url)
+;;    (("X" . "Cut Sexp") . thing-cut-sexp)
+;;    (("G" . "Cut Page") . thing-cut-page)
+;;    (("T" . "Cut Sentence") . thing-cut-sentence)
+;;    (("O" . "Cut Whitespace") . thing-cut-whitespace)
+;;    (("I" . "Cut List") . thing-cut-list)
+;;    (("C" . "Cut Comment") . thing-cut-comment)
+;;    (("H" . "Cut Function") . thing-cut-defun)
+;;    (("P" . "Cut Parentheses") . thing-cut-parentheses)
+;;    (("L" . "Cut Line") . thing-cut-line)
+;;    (("A" . "Cut To Line Begin") . thing-cut-to-line-beginning)
+;;    (("E" . "Cut To Line End") . thing-cut-to-line-end)
+;;    )
+;;  t)
 
 
 ;; (use-package esup
